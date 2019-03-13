ba4c1761b7d76337ba631c647aa34f303ac4aadd
DarkoKaren <48030845+DarkoKaren@users.noreply.github.com>
2019-03-13 15:17:46 +0100

    New

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using ConsoleApp1;
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User[] users = new User[]
+           {
+               new User("Darko","345"),
+               new User("Ivana","123"),
+               new User("Angel","567")
+           };
+
+            Console.WriteLine("Enter a 1 on 2 : 1 is Login, 2 is Register");
+
+            var input = Console.ReadLine();
+
+            while (true)
+            {
+                if (input != "1" || input != "2")
+                {
+                    Console.WriteLine("Invalid input of number");
+                    break;
+                }
+            }
+
+            if (input == "1")
+            {
+                Console.WriteLine("Enter a user");
+                var riduse = Console.ReadLine();
+
+                Console.WriteLine("Enter a pasword");
+                var ridpassword = Console.ReadLine();
+
+                foreach (var user in users)
+                {
+                    if (user.Login == riduse && user.Pasword == ridpassword)
+                    {
+                        Console.WriteLine("Well you'r user is corect  WELCOME");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalit User or Password");
+                    }
+                }
+
+
+            }
+            else if (input == "2")
+            {
+                Console.WriteLine("Enter your new login user");
+                var newuse = Console.ReadLine();
+
+                Console.WriteLine("Enter a your new pasword");
+                var newpassword = Console.ReadLine();
+
+                Array.Resize(ref User, User.Length + 1);
+                User[User.Length - 1] = new User(username, password);
+
+
+            }
+            //Console.WriteLine($"Your user is: {user} ,End your pasword is:***");
+            Console.ReadLine();
+
+        }
+    }
+}
+    
---
+++ ConsoleApp1/ConsoleApp1/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class User
+    {
+        public string Username;
+        public string Pasword;
+
+        public User(string username, string pasword)
+        {
+            Username = username;
+            Pasword = pasword;
+        }
+    }
+}
