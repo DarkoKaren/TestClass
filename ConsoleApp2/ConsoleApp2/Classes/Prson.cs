6ff704e891ae50589bcb1d8dcf45e30760ca8e2b
DarkoKaren <48030845+DarkoKaren@users.noreply.github.com>
2019-03-27 00:48:51 +0100

    Revert "Revert "CondolApp2""
    This reverts commit 20a1828258354be4a2f4fae1179fe8ec62ec9384.

---
+++ ConsoleApp2/ConsoleApp2/Classes/Prson.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp2.Classes
+{
+   public class Prson
+    {
+
+        public String Name { set; get; }
+        public String LastName { set; get; }
+        public int Age;
+        private string v1;
+        private string v2;
+        private int v3;
+        private string v4;
+
+        public Prson(string v1, string v2, int v3, string v4)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+            this.v3 = v3;
+            this.v4 = v4;
+        }
+
+        public String Works { set; get; }
+    }
+}
